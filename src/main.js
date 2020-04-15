15444199e190b13b4d6a6d10b255a16fc7fb73a5
Hewwho <50575563+Hewwho@users.noreply.github.com>
2020-04-15 03:24:39 +0200

    Changed camera rotation to objects rotation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { DragControls } from './three.js-master/examples/jsm/controls/DragControls.js';
-import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
 
 
 var renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
@@ -19,6 +18,8 @@ scene.add( new THREE.AmbientLight( 0xD9D9D9 ) );
 
 // PALLET
 
+var palletGroup = new THREE.Group();
+
 const palletWidth = 20;
 const palletHeight = 40;
 const palletDepth = 1;
@@ -26,27 +27,31 @@ var pallet = new THREE.Mesh(
     new THREE.BoxGeometry( palletWidth, palletHeight, palletDepth ), 
     new THREE.MeshBasicMaterial( { color: 0x888888 } )
 );
+
+// y = - palletDepth / 2, so that its upper surface is equal to y = 0
 pallet.position.set( 0, - palletDepth / 2, 0 );
 pallet.rotation.x = 90 * Math.PI / 180;
-scene.add( pallet );
+palletGroup.add(pallet);
+scene.add( palletGroup );
 
 var palletFrame = new THREE.LineSegments(
-
     new THREE.EdgesGeometry( pallet.geometry ), 
     new THREE.LineBasicMaterial( { color: 0x303030, linewidth: 1 } )
-
 );
 pallet.add( palletFrame );
 
 
-//USED FOR CHECKING WHETHER A PACKAGE IS PLACED WITHIN THE PALLET'S BOUNDARIES
+// USED FOR CHECKING WHETHER A PACKAGE IS PLACED WITHIN THE PALLET'S BOUNDARIES
 
-function isPackageWithinPallet( obj ) {
+function isPackageWithinPallet( packPos, packGeoParam ) {
 
-    if( obj.position.x - obj.geometry.parameters.width / 2 >= centralPoint.x - pallet.geometry.parameters.width / 2   && 
-        obj.position.x + obj.geometry.parameters.width / 2 <= centralPoint.x + pallet.geometry.parameters.width / 2   &&
-        obj.position.z - obj.geometry.parameters.height / 2 >= centralPoint.z - pallet.geometry.parameters.height / 2 && 
-        obj.position.z + obj.geometry.parameters.height / 2 <= centralPoint.z + pallet.geometry.parameters.height / 2
+    let palletGeoParam = pallet.geometry.parameters;
+
+    if(
+        packPos.x - packGeoParam.width / 2 >= centralPoint.x - palletGeoParam.width / 2   && 
+        packPos.x + packGeoParam.width / 2 <= centralPoint.x + palletGeoParam.width / 2   &&
+        packPos.z - packGeoParam.height / 2 >= centralPoint.z - palletGeoParam.height / 2 && 
+        packPos.z + packGeoParam.height / 2 <= centralPoint.z + palletGeoParam.height / 2
     ) {
 
         return true;
@@ -65,7 +70,9 @@ function isPackageWithinPallet( obj ) {
 var draggableObjects = [];
 
 class Package extends THREE.Mesh {
+
     constructor( obj ) {
+
         super(
             new THREE.BoxGeometry( obj.width, obj.height, obj.depth ),
             new THREE.MeshPhongMaterial( { color: obj.color } )
@@ -74,14 +81,16 @@ class Package extends THREE.Mesh {
         this.originalPos = new THREE.Vector3( obj.posX, obj.posY, obj.posZ );
         this.lastPos = this.originalPos;
 
+        // doesn't do anything for now
         this.weight = obj.weight;
 
         this.position.set( obj.posX, obj.posY, obj. posZ );
         this.rotation.x = 90 * Math.PI / 180;
 
+        // was the package's last, static position on the sidebar?
         this.wasOnTheSidebar = false;
 
-        scene.add( this );
+        palletGroup.add( this );
         draggableObjects.push( this );
 
     }
@@ -113,7 +122,7 @@ var package1 = new Package( {
     width:  4,
     height: 4,
     depth:  4,
-    color:  0xff0000,
+    color:  0xFF0000,
     weight: 0
 
 } );
@@ -126,15 +135,13 @@ var package2 = new Package( {
     width:  6,
     height: 4,
     depth:  3,
-    color:  0x0000ff,
+    color:  0x0000FF,
     weight: 0
 
 } );
 
 
-//PERSPECTIVE CAMERA AND ITS CONTROLS
-
-var sceneUI = new THREE.Scene();
+// PERSPECTIVE CAMERA
 
 var cameraPerp = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
 scene.add( cameraPerp );
@@ -142,13 +149,94 @@ scene.add( cameraPerp );
 cameraPerp.position.set( 0, 40, 80 );
 cameraPerp.lookAt( pallet.position );
 
-var cameraPerpControls = new OrbitControls( cameraPerp, renderer.domElement );
-cameraPerpControls.mouseButtons = {
-	RIGHT: THREE.MOUSE.ROTATE
-}
+
+// PALLETGROUP ROTATION CONTROLS
+
+var lmbDown = false;
+
+window.addEventListener( 'mousedown', function ( e ) {
+
+    if (e.button == 0 ) {
+
+        lmbDown = true;
+
+    }
+
+}, false );
+
+window.addEventListener( 'mouseup', function ( e ) {
+
+    if( e.button == 0 ) {
+
+        lmbDown = false;
+
+    }
+
+}, false );
+
+var rmbDown = false,
+    mouseX = 0,
+    mouseY = 0;
+
+window.addEventListener( 'mousemove', function ( e ) {
+
+    if ( !rmbDown ) {
+
+        return;
+
+    }
+
+    e.preventDefault();
+
+    var deltaX = e.clientX - mouseX,
+        deltaY = e.clientY - mouseY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    rotateScene( deltaX, deltaY );
+
+}, false );
+
+window.addEventListener( 'mouseup', function ( e ) {
+
+    // if rmb is pressed and lmb is not pressed to prevent dragging and rotating at the same time
+    if( e.button == 2 && ! lmbDown ) {
+
+        e.preventDefault();
+
+        rmbDown = false;
+
+    }
+
+}, false );
+
+window.addEventListener( 'mousedown', function ( e ) {
+
+    // if rmb is pressed and lmb is not pressed to prevent dragging and rotating at the same time
+    if( e.button == 2 && ! lmbDown ) {
+
+        e.preventDefault();
+
+        rmbDown = true;
+
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+
+    }
+}, false );
 
 
-//SIDEBAR
+
+function rotateScene( deltaX, deltaY ) {
+
+    palletGroup.rotation.y += deltaX / 100;
+    palletGroup.rotation.x += deltaY / 100;
+
+} 
+
+
+// SIDEBAR
+
+var sceneUI = new THREE.Scene();
 
 var cameraOrth = new THREE.OrthographicCamera( -100, 100, 100, -100, -1, 0 );
 cameraOrth.position.set( 0, 0, 0 );
@@ -162,11 +250,13 @@ var sidebar = new THREE.Line(
 
     new THREE.BufferGeometry().setFromPoints( sidebarPoints ), 
     new THREE.LineDashedMaterial( {
+
         color: 0x404040, 
         linewidth: 1,
         scale: 0.5,
         dashSize: 1.5,
         gapSize: 2
+
     } )
 
 );
@@ -174,7 +264,7 @@ sidebar.computeLineDistances();
 sceneUI.add( sidebar );
 
 
-//CURRENT MOUSE POSITION
+// CURRENT MOUSE POSITION
 
 var mouse = new THREE.Vector2();
 
@@ -206,35 +296,53 @@ dragControls.addEventListener( 'hoveroff', function( event ) {
 
 } );
 
-dragControls.addEventListener( 'dragstart', function( event ) {
-
-    let pack = event.object;
-
-    if( pack.wasOnTheSidebar ) {
-        
-        //
-
-    }
-    else {
-
-        //
-
-    }
-    
-    
-} );
-
 dragControls.addEventListener( 'dragend', function ( event ) {
     
     let pack = event.object;
 
+    // moved onto the sidebar
     if( mouse.x * 100 >= sidebarX ) {
 
         if( !pack.wasOnTheSidebar ) {
 
-            // TEMPORARY WORKDAROUND
-            cameraPerp.attach( pack );
-            //
+            let worldPosition = pack.position;
+            palletGroup.localToWorld( worldPosition );
+
+            // to prevent it from flickering momentarily when adding to scene
+            pack.matrixAutoUpdate = false;
+
+            // removing the pack from the pallet by adding it to the scene, so that it doesn't
+            // start rotating around the pallet's center
+            scene.add(pack);
+
+            let palletGroupRotation = new THREE.Euler();
+            palletGroupRotation.copy( palletGroup.rotation );
+
+            // all of this because the pallet is on its side
+            palletGroupRotation.x += 90 * Math.PI / 180;
+            let y = - palletGroupRotation.y;
+            palletGroupRotation.y = palletGroupRotation.z;
+            palletGroupRotation.z = y;
+
+            pack.rotation.copy( palletGroupRotation );
+
+            pack.position.copy( worldPosition );
+            pack.matrixAutoUpdate = true;
+
+            // copying the pallet's rotation with every frame
+            pack.onBeforeRender = () => {
+
+                let palletGroupRotation = new THREE.Euler();
+                palletGroupRotation.copy( palletGroup.rotation );
+
+                palletGroupRotation.x += 90 * Math.PI / 180;
+                let y = - palletGroupRotation.y;
+                palletGroupRotation.y = palletGroupRotation.z;
+                palletGroupRotation.z = y;
+
+                pack.rotation.copy( palletGroupRotation );
+                
+            }
 
             pack.sidebarOrNot();
 
@@ -242,22 +350,31 @@ dragControls.addEventListener( 'dragend', function ( event ) {
 
         pack.updateLastPos();
 
-    } else {
+    
+    } 
+    // moved onto the pallet / empty space
+    else {
 
         if( pack.wasOnTheSidebar ) {
 
-            // TEMPORARY WORKAROUND
-            scene.add( pack );
-            pack.position.z += cameraPerp.position.length();
-            //
+            // if it was on the sidebar (not part of palletGroup), then we need to translate its world position
+            // to palletGroup's local position to compare it to pallet's boundaries
+            let packLocalPosition = pack.position;
+            palletGroup.worldToLocal( packLocalPosition );
 
-            if( isPackageWithinPallet( pack ) ) {
-
-                //
+            if( isPackageWithinPallet( packLocalPosition, pack.geometry.parameters ) ) {
 
                 pack.updateLastPos();
+
+                palletGroup.add( pack );
+                
+                pack.onBeforeRender = () => {
+    
+                }
+
+                pack.rotation.copy( pallet.rotation );
+
                 pack.sidebarOrNot();
-                pack.rotation.copy(pallet.rotation);
 
             } else {
 
@@ -268,9 +385,7 @@ dragControls.addEventListener( 'dragend', function ( event ) {
         }
         else {
 
-            if( isPackageWithinPallet( pack ) ) {
-
-                //
+            if( isPackageWithinPallet( pack.position, pack.geometry.parameters ) ) {
 
                 pack.updateLastPos();
 
@@ -286,7 +401,7 @@ dragControls.addEventListener( 'dragend', function ( event ) {
 } );
 
 
-//RENDERING
+// RENDERING
 
 render();
 
